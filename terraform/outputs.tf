3f66516ccec3d23daa3aa9b7f33d83a0073e3d73
ma914372 <45353975+ma914372@users.noreply.github.com>
2025-02-14 15:33:56 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,9 +6,6 @@ output "worker_node_ips" {
   value = join(",", [for ip in aws_instance.kubernetes_worker_nodes : ip.public_ip])
 }
 
-output "ansible_node_ip" {
-  value = aws_instance.ansible_node.public_ip
-}
 output "master_private_ip" {
   value = aws_instance.kubernetes_master.private_ip
 }
